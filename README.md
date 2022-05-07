a9129ac018a83854eefe156f408da50df542c5bc
donno2048 <just4now666666@gmail.com>
2022-05-08 02:16:12 +0300

    add webassembly

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # pyas
 
-Run machine code and assembly directly in Python
+Run machine code, assembly and webassembly directly in Python.
 
 Have you ever realized you can't remember how to add numbers in Python but you **do** remember how to do it in assembly?
 
-If the answer is yes this is the package for you, using `pyas` you will be able to run machine code and assembly directly from within Python!
+If the answer is yes this is the package for you, using `pyas` you will be able to run machine code, assembly and webassembly directly from within Python!
 
 ```diff
 - currently this only supports Linux
@@ -99,4 +99,20 @@ add_numbers = lambda i, val: function(
 print(add_numbers(4, 10), "=", "10 + 4")
 # output: 14 = 10 + 4
 
+```
+
+## WebAssembly
+
+pyas will automatically recognize running in a web browser and will run Webassembly, but you'll have to specify a `func_name`.
+
+```py
+from pyas import function
+add_one = function(
+    '''
+    \x00asm\x01\x00\x00\x00\x01\x06\x01`
+    \x01\x7f\x01\x7f\x03\x02\x01\x00\x07
+    \x0b\x01\x07add_one\x00\x00\n\t\x01
+    \x07\x00\x20\x00A\x01j\x0b
+    ''', func_name = 'add_one')
+print(str(add_one(10)) + "=" + str(add_one(9)) + "+ 1")
 ```
--- pyas/__init__.py
+++ pyas/__init__.py
@@ -1,26 +1,38 @@
 from ctypes import CFUNCTYPE, addressof, c_void_p as void
-from tempfile import NamedTemporaryFile
+from platform import system as platform
 from binascii import unhexlify
-from mmap import mmap
-from os import system
 buffer = []
 def __strip_comments(string, separators):
 	for sep in separators: string = string.split(sep)[0]
 	return string
-def function(asm: str, /, *args, raw = True):
+def function(asm: str, /, *args, raw = True, func_name = "main"):
 	global buffer
 	if args: return function(asm, raw=raw)(*args)
-	if raw: asm_parsed = unhexlify(str().join(chunk.strip() for l in asm.splitlines() for chunk in __strip_comments(l, ["#", ";", "//"]).split()))
+	if platform() == "Linux":
+		from mmap import mmap
+		if raw: asm_parsed = unhexlify(str().join(chunk.strip() for l in asm.splitlines() for chunk in __strip_comments(l, ["#", ";", "//"]).split()))
+		else:
+			from tempfile import NamedTemporaryFile
+			from os import system
+			temp = NamedTemporaryFile(mode='w+t', delete=False)
+			temp.write(asm)
+			temp.close()
+			name = temp.name
+			if not system("nasm -felf {} -o {}.o".format(name, name)): # this isn't cheating, I'm just checking what is the raw machine code to be able to use it
+				system("objcopy -O binary {}.o {}.hex".format(name, name))
+				try: asm_parsed = open("{}.hex".format(name), "rb").read()
+				except FileNotFoundError: return None
+			else: return None
+		buffer += [mmap(-1, length=max(len(asm_parsed)//2, 8), prot=7)]
+		for byte in asm_parsed: buffer[-1].write_byte(byte)
+		return CFUNCTYPE(void)(addressof(void.from_buffer(buffer[-1])))
+	elif platform() == "Emscripten":
+		from pyodide_js._module import wasmTable
+		from js import eval, WebAssembly
+		from pyodide import to_js
+		ptr = wasmTable.grow(1)
+		ins = WebAssembly.Instance.new(WebAssembly.Module.new(to_js(asm.encode())))
+		wasmTable.set(ptr, eval(f"(_) => _.exports.{func_name}")(ins))
+		return CFUNCTYPE(void)(ptr)
 	else:
-		temp = NamedTemporaryFile(mode='w+t', delete=False)
-		temp.write(asm)
-		temp.close()
-		name = temp.name
-		if not system("nasm -felf {} -o {}.o".format(name, name)): # this isn't cheating, I'm just checking what is the raw machine code to be able to use it
-			system("objcopy -O binary {}.o {}.hex".format(name, name))
-			try: asm_parsed = open("{}.hex".format(name), "rb").read()
-			except FileNotFoundError: return None
-		else: return None
-	buffer += [mmap(-1, length=max(len(asm_parsed)//2, 8), prot=7)]
-	for byte in asm_parsed: buffer[-1].write_byte(byte)
-	return CFUNCTYPE(void)(addressof(void.from_buffer(buffer[-1])))
+		return None

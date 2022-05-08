04cdf2ba2291e36ef6a2cded254504bc05b1bd98
Elisha Hollander <just4now666666@gmail.com>
2022-05-08 19:51:40 +0300

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='pyas',
-    version='1.2.1',
+    version='1.2.2',
     license='MIT',
     author='Elisha Hollander',
     author_email='just4now666666@gmail.com',
